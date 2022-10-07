e57a14fdb10f8c7c4b9e333ca162c3a79b6095f4
Tim Schneeberger <tim.schneeberger@outlook.de>
2022-10-07 02:38:06 +0200

    Add X-Limit header

--- Controllers/ResultController.cs
+++ Controllers/ResultController.cs
@@ -58,9 +58,11 @@ public class ResultController : ControllerBase
     {
         if (!IsKnownClient())
             return NotFound();
-        
-        var results = AeqIndexCache.Search(query, 50, out var isPartialBool);
+
+        const int limit = 50;
+        var results = AeqIndexCache.Search(query, limit, out var isPartialBool);
         Response.Headers.Add("X-Partial-Result", isPartialBool ? "1" : "0");
+        Response.Headers.Add("X-Limit", limit.ToString());
         return results;
     }
 }
